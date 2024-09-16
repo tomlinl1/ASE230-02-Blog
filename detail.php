fb44aeaba76c7fdc287751b430a8db4bca0092cf
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-09-16 19:15:52 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,3 +1,33 @@
 <?php
 $i=$_GET['post_id'];
+<<<<<<< Updated upstream
 ?>
+=======
+$blogs = [
+    [
+        'title'=> 'Gaming',
+        'content' => 'Lipsum',
+        'author' => 'Landen Tomlin',
+        'date' => '9/16/2024'
+    ],
+    [
+        'title'=> 'Weightlifting',
+        'content' => 'Lipsum',
+        'author' => 'Josh Harris',
+        'date' => '9/14/2024'
+    ],
+    [
+        'title'=> 'Cybersecurity',
+        'content' => 'Lipsum',
+        'author' => 'Michael Hoffmeier',
+        'date' => '9/15/2024'
+    ]
+]
+?>
+
+<html>
+    <head>
+        <title><?=$blogs[$i]['title'].' by '.$blogs[$i]['author']?></title>
+    </head>
+</html>
+>>>>>>> Stashed changes
