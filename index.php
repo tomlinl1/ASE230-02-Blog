cc28f44ed39cdbf593f0f34441af02eca4a66897
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-09-16 20:55:50 -0400

    Bootstrap?

--- index.php
+++ index.php
@@ -32,21 +32,32 @@ $blogs = [
         ]
     ]
 ?>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Amazing Team Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
-    </head>
+
+    <div class="p-3 mb-2 bg-secondary text-white">
     <body>
+    <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
         <h1>Our Blogs</h1>
-        <?php for($i=0;$i<count($blogs); $i++) { ?>
-            <ul>
-            <?= '<li><a href="detail.php?post_id='.$i.'">'.$blogs[$i]['title'].'</a></li>'?>
-        </ul>
-        <?php } ?>
+    </div>
+    <div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col">
+                <?php for($i=0;$i<count($blogs); $i++) { ?> 
+                    <div class="p-5 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+                        <?= '<h3><a href="detail.php?post_id='.$i.'">'.$blogs[$i]['title'].'</a></h3>'?>
+                    </div>        
+                <?php } ?>
+            </div>
+        </div>
+    </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
+    </div>
 </html>
