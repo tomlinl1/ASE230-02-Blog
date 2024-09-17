e56db78e212da21d214501384536613a519d4aaf
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-09-17 11:16:16 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -49,7 +49,7 @@ $blogs = [
             <h3><?='by: '.$blogs[$i]['author']?></h3>
             <h4><?='Posted on: '.$blogs[$i]['date']?></h4>
             <p><?=$blogs[$i]['content']?></p>
-            <button type="button" class="btn btn-light"><a href="index.php">Return To Home</a></button>
+            <button type="button" class="btn btn-light"><a style="text-decoration: none" href="index.php">Return To Home</a></button>
         </div>
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
