83bf2bb1b1ece33c227c1b190a3b595db7946a75
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-09-17 11:08:32 -0400

    Styling with Bootstrap

--- detail.php
+++ detail.php
@@ -34,20 +34,25 @@ $blogs = [
 ]
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="en" class="p-3 mb-2 bg-secondary text-white">
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$blogs[$i]['title'].' by '.$blogs[$i]['author']?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
-    <div class="p-3 mb-2 bg-secondary text-white">
-    <body>
-        <h1><?=$blogs[$i]['title']?></h1>
-        <h3><?='by: '.$blogs[$i]['author']?></h3>
-        <h4><?='Posted on: '.$blogs[$i]['date']?></h4>
-        <p><?=$blogs[$i]['content']?></p>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
+    
+    <body class="p-3 mb-2 bg-secondary text-white">
+    <div class="container">
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+            <h1><?=$blogs[$i]['title']?></h1>
+            <h3><?='by: '.$blogs[$i]['author']?></h3>
+            <h4><?='Posted on: '.$blogs[$i]['date']?></h4>
+            <p><?=$blogs[$i]['content']?></p>
+            <button type="button" class="btn btn-light"><a href="index.php">Return To Home</a></button>
+        </div>
     </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    </body>
 </html>
--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $blogs = [
     ]
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="en" class="p-3 mb-2 bg-secondary text-white">
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -41,8 +41,8 @@ $blogs = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
 
-    <div class="p-3 mb-2 bg-secondary text-white">
-    <body>
+    
+    <body class="p-3 mb-2 bg-secondary text-white">
     <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
         <h1>Our Blogs</h1>
     </div>
@@ -59,5 +59,4 @@ $blogs = [
     </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
-    </div>
 </html>
